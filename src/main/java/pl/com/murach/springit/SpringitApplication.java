7a3b8227407d13fe4d0c0cf5ecbeb2cbfd556124
Przemek <syrtismajor@gmail.com>
2022-04-03 15:41:23 +0200

    Creating entities

--- src/main/java/pl/com/murach/springit/SpringitApplication.java
+++ src/main/java/pl/com/murach/springit/SpringitApplication.java
@@ -2,13 +2,17 @@ package pl.com.murach.springit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pl.com.murach.springit.config.SpringitProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 		System.out.println("Welcome to Springit!");
+		System.out.println("Dude");
 	}
 
 }
